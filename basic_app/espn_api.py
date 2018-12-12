8420d7dcda5227d37def3960864a016b89ba32fc
Trent Frederick <trentfrederick@gmail.com>
2018-12-12 13:53:59 -0600

    syntax change

--- basic_app/espn_api.py
+++ basic_app/espn_api.py
@@ -123,8 +123,7 @@ def gw_db_update(gw):
 db_load = cur_db(gw)
 db_update = gw_db_update(gw)
 
-clist = list(CurrentSeason.objects.values_list('game_week', 'team_name', 'team_abbrev', 'poinst_for',
-'opponent', 'points_against', 'result')
+clist = list(CurrentSeason.objects.values_list('game_week', 'team_name', 'team_abbrev', 'poinst_for', 'opponent', 'points_against', 'result'))
 cols = ['Week', 'Team Name', 'Abbrev', 'Score', 'Opponent', 'Points against', 'Result']
 df = pd.DataFrame.from_records(clist, columns=cols)
 
