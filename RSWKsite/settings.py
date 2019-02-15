d3b7db72e6e0cd78204d62452ce13332399c94d9
Trent Frederick <trentfrederick@gmail.com>
2019-02-15 14:56:02 -0600

    more bugz

--- RSWKsite/settings.py
+++ RSWKsite/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'basic_app_test.context_processors.add_variable_to_context',
+                'basic_app.context_processors.add_variable_to_context',
             ],
         },
     },
