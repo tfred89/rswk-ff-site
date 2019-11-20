e93f58118e5e5a9b46e268cedf59d15f2442b16f
Trent Frederick <tfrederick@restreamsolutions.com>
2019-11-20 00:06:17 -0600

    admin added

--- basic_app/admin.py
+++ basic_app/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
 from basic_app.models import PastSeasons, CurrentSeason, Player, Rankings, Skittish
 
+
+class RankingsA(admin.ModelAdmin):
+    list_display = ['year', 'game_week', 'team_name', 'owner',
+                    'place', 'wins', 'losses']
+    search_fields = ['game_week', 'owner']
+
+
+class PastSeasonsA(admin.ModelAdmin):
+    list_display = ['year', 'place', 'team_name',
+                    'owner', 'wins', 'losses', 'ties']
+    search_fields = ['owner', 'year']
+
+
+class CSA(admin.ModelAdmin):
+    list_display = ['year', 'game_week', 'team_name', 'points',
+                    'points_against', 'owner', 'point_dif']
+    search_fields = ['owner', 'team_name']
+
+
+class SkittishA(admin.ModelAdmin):
+    list_display = ['player', 'eiliminated']
+    search_fields = ['player']
+
+
+
 # Register your models here.
-admin.site.register(PastSeasons)
-admin.site.register(CurrentSeason)
+admin.site.register(PastSeasons, PastSeasonsA)
+admin.site.register(CurrentSeason, CSA)
 admin.site.register(Player)
-admin.site.register(Rankings)
-admin.site.register(Skittish)
+admin.site.register(Rankings, RankingsA)
+admin.site.register(Skittish, SkittishA)
