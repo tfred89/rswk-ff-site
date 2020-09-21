89ef2e439bce4df85b7390957ea310e357e359fb
Trent Frederick <trentfrederick@gmail.com>
2020-09-21 17:15:11 -0500

    prod settings change

--- RSWKsite/settings.py
+++ RSWKsite/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# django_heroku.settings(locals())
+STATIC_ROOT = '/static/'
+django_heroku.settings(locals())
 
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
